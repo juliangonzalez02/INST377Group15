7784106e6522556abb66258e9edc5613bf787553
Julian Gonzalez <julian.f.gonzalez02@gmail.com>
2024-05-18 13:49:58 -0400

    Added markers for available bike stations

--- public/nearby_bikes.js
+++ public/nearby_bikes.js
@@ -5,11 +5,12 @@ const universityCoords = { lat: 38.9869, lng: -76.9426 }; // University of Maryl
 
 // Function to fetch nearby places using Google Maps Places API
 async function fetchNearbyPlaces() {
-    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${universityCoords.lat},${universityCoords.lng}&radius=2000&type=${placeType}&key=${apiKey}`;
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${universityCoords.lat},${universityCoords.lng}&radius=3000&type=${placeType}&key=${apiKey}`;
 
     try {
         const response = await fetch(url);
         const data = await response.json();
+        console.log(data);
         return data.results;
     } catch (error) {
         console.error('Error fetching nearby places:', error);
@@ -19,10 +20,19 @@ async function fetchNearbyPlaces() {
 
 // Function to display the results
 function displayResults(places) {
-    console.log('Shared e-bikes and scooters around University of Maryland College Park:');
+    var map = L.map("map").setView([38.9869, -76.9426], 13);
+    //console.log('Shared e-bikes and scooters around University of Maryland College Park:');
     places.forEach((place, index) => {
-        console.log(`${index + 1}. ${place.name} - ${place.vicinity}`);
+        //console.log(`${index + 1}. ${place.name} - ${place.vicinity} - ${place.geometry.location.lat} - ${place.geometry.location.lng}`);
+        var marker = L.marker([place.geometry.location.lat, place.geometry.location.lng]).addTo(map);
+        marker.bindPopup(`${place.vicinity}`).openPopup();
     });
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 15,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
 }
 
 // Main function
